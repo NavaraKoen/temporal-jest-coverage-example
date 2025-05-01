61c52feed57784666055f3308c4d6a3150df8d28
Koen Griffioen <koen.griffioen@essent.nl>
2025-05-01 17:25:56 +0200

    add mocha test example

---
+++ src/mocha/workflows-with-coverage.test.ts
@@ -0,0 +1,48 @@
+import { TestWorkflowEnvironment } from '@temporalio/testing';
+import { Worker } from '@temporalio/worker';
+import { after } from 'mocha';
+import { example } from '../workflows';
+import * as activities from '../activities';
+import assert from 'assert';
+import { WorkflowCoverage } from '@temporalio/nyc-test-coverage';
+
+const workflowCoverage = new WorkflowCoverage();
+
+describe('Example workflow', () => {
+  let testEnv: TestWorkflowEnvironment;
+
+  beforeEach(async () => {
+    testEnv = await TestWorkflowEnvironment.createLocal();
+  });
+
+  afterEach(async () => {
+    await testEnv?.teardown();
+  });
+
+  it('successfully completes the Workflow', async () => {
+    const { client, nativeConnection } = testEnv;
+    const taskQueue = 'test';
+
+    const worker = await Worker.create(
+      workflowCoverage.augmentWorkerOptions({
+        connection: nativeConnection,
+        taskQueue,
+        workflowsPath: require.resolve('../workflows'),
+        activities,
+      }),
+    );
+
+    const result = await worker.runUntil(
+      client.workflow.execute(example, {
+        args: ['Temporal'],
+        workflowId: 'test',
+        taskQueue,
+      }),
+    );
+    assert.equal(result, 'Hello, Temporal!');
+  });
+});
+
+after(() => {
+  workflowCoverage.mergeIntoGlobalCoverage();
+});
--- src/workflows.test.ts
+++ src/workflows.test.ts
@@ -1,4 +1,5 @@
 import { TestWorkflowEnvironment } from '@temporalio/testing';
+import { WorkflowCoverage } from '@temporalio/nyc-test-coverage';
 import { Worker } from '@temporalio/worker';
 import { example } from './workflows';
 import * as activities from './activities';
@@ -15,24 +16,58 @@ describe('Example workflow', () => {
     await testEnv?.teardown();
   });
 
-  it('successfully completes the Workflow', async () => {
-    const { client, nativeConnection } = testEnv;
-    const taskQueue = 'test';
+  const taskQueue = 'test';
 
-    const worker = await Worker.create({
-      connection: nativeConnection,
-      taskQueue,
-      workflowsPath: require.resolve('./workflows'),
-      activities,
+  describe('without nyc coverage patch (no coverage)', () => {
+    it('successfully completes the Workflow', async () => {
+      const { client } = testEnv;
+
+      const { nativeConnection } = testEnv;
+      const worker = await Worker.create({
+        connection: nativeConnection,
+        taskQueue,
+        workflowsPath: require.resolve('./workflows'),
+        activities,
+      });
+
+      const result = await worker.runUntil(
+        client.workflow.execute(example, {
+          args: ['Temporal'],
+          workflowId: 'test',
+          taskQueue,
+        }),
+      );
+      assert.equal(result, 'Hello, Temporal!');
+    });
+  });
+
+  describe('with nyc coverage patch (erratic coverage)', () => {
+    const workflowCoverage = new WorkflowCoverage();
+    it('successfully completes the Workflow', async () => {
+      const { client } = testEnv;
+
+      const { nativeConnection } = testEnv;
+      const worker = await Worker.create(
+        workflowCoverage.augmentWorkerOptions({
+          connection: nativeConnection,
+          taskQueue,
+          workflowsPath: require.resolve('./workflows'),
+          activities,
+        }),
+      );
+
+      const result = await worker.runUntil(
+        client.workflow.execute(example, {
+          args: ['Temporal'],
+          workflowId: 'test',
+          taskQueue,
+        }),
+      );
+      assert.equal(result, 'Hello, Temporal!');
     });
 
-    const result = await worker.runUntil(
-      client.workflow.execute(example, {
-        args: ['Temporal'],
-        workflowId: 'test',
-        taskQueue,
-      }),
-    );
-    assert.equal(result, 'Hello, Temporal!');
+    afterAll(async () => {
+      workflowCoverage.mergeIntoGlobalCoverage();
+    });
   });
 });
